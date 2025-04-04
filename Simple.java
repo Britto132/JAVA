3393fee0af6a9ec746eb7b4cdcfaeba24f18a61c
sudhamurari <sudhamurari74@gmail.com>
2025-04-04 09:21:34 +0530

    Save this addition

---
+++ Simple.java
@@ -0,0 +1,17 @@
+import java.util.Scanner
+public class Simple {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Yoyr First Number:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter Your Second  Number:");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Addition is:" +(num1+num2));
+
+        
+    }
+
+    
+}
